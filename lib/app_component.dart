352381b6fb7e941fa766f70017779ee77be69f08
rinukkusu <rinukkusu@sub-r.de>
2017-06-23 11:43:53 +0200

    first commit

---
+++ lib/app_component.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2017, 'rinukkusu'. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/angular2.dart';
+import 'package:angular_components/angular_components.dart';
+
+@Component(
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: const [materialDirectives],
+  providers: const [materialProviders],
+)
+class AppComponent {
+  ScoreboardType type = ScoreboardType.toggle;
+  
+  bool card1 = false;
+  bool card2 = false;
+  bool card3 = false;
+}
